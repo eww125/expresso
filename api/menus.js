e9046c4c3bb5116ad938d05568ff3ef2ac3fadfc
Evan Wappel <ewappel@ewappel-macbookpro.roam.corp.google.com>
2017-11-28 17:15:55 -0800

    routes starting to work

--- api/menus.js
+++ api/menus.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const menuRouter = express.Router();
+const menusRouter = express.Router();
 
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
-const issuesRouter = require('./issues.js');
-
-menuRouter.param('menuId', (req, res, next, menuId) => {
-  const sql = 'SELECT * FROM Menu WHERE Menu.id = $MenuId';
+menusRouter.param('menuId', (req, res, next, menuId) => {
+  const sql = 'SELECT * FROM Menu WHERE Menu.id = $menuId';
   const values = {$menuId: menuId};
   db.get(sql, values, (error, menu) => {
     if (error) {
@@ -21,33 +19,34 @@ menuRouter.param('menuId', (req, res, next, menuId) => {
   });
 });
 
-menuRouter.use('/:menuId/issues', issuesRouter);
-
-menuRouter.get('/', (req, res, next) => {
-  db.all('SELECT * FROM Menu', (err, menu) => {
-    if (err) {
-      next(err);
-    } else {
-      res.status(200).json({menu: menu});
-    }
-  });
+menusRouter.get('/', (req, res, next) => {
+  console.log('menusRouter.get/')
+  db.all('SELECT * FROM Menu',
+    (err, menus) => {
+      if (err) {
+        next(err);
+      } else {
+        res.status(200).json({menus: menus});
+      }
+    });
 });
 
-menuRouter.get('/:menuId', (req, res, next) => {
+menusRouter.get('/:menuId', (req, res, next) => {
+  console.log('menusRouter.get/:menuId')
   res.status(200).json({menu: req.menu});
 });
 
-menuRouter.post('/', (req, res, next) => {
-  const name = req.body.menu.name,
-        description = req.body.menu.description;
-  if (!name || !description) {
+menusRouter.post('/', (req, res, next) => {
+  console.log('menusRouter.post/')
+  const title = req.body.menu.title
+  if (!title) {
     return res.sendStatus(400);
   }
 
-  const sql = 'INSERT INTO Menu (name, description) VALUES ($name, $description)';
+  const sql = 'INSERT INTO Menu (title, id)' +
+      'VALUES ($title, $id)';
   const values = {
-    $name: name,
-    $description: description
+    $title: title
   };
 
   db.run(sql, values, function(error) {
@@ -62,18 +61,18 @@ menuRouter.post('/', (req, res, next) => {
   });
 });
 
-menuRouter.put('/:menuId', (req, res, next) => {
-  const name = req.body.menu.name,
-        description = req.body.menu.description;
-  if (!name || !description) {
+menusRouter.put('/:menuId', (req, res, next) => {
+  console.log('menusRouter.put/')
+  console.log('menuId=' + req.params.menuId)
+  const title = req.body.menu.title
+  if (!title) {
     return res.sendStatus(400);
   }
 
-  const sql = 'UPDATE Menu SET name = $name, description = $description ' +
+  const sql = 'UPDATE Menu SET title = $title ' +
       'WHERE Menu.id = $menuId';
   const values = {
-    $name: name,
-    $description: description,
+    $title: title,
     $menuId: req.params.menuId
   };
 
@@ -89,27 +88,20 @@ menuRouter.put('/:menuId', (req, res, next) => {
   });
 });
 
-menuRouter.delete('/:menuId', (req, res, next) => {
-  const issueSql = 'SELECT * FROM Issue WHERE Issue.menu_id = $menuId';
-  const issueValues = {$menuId: req.params.menuId};
-  db.get(issueSql, issueValues, (error, issue) => {
+menusRouter.delete('/:menuId', (req, res, next) => {
+  const sql = 'UPDATE Menu SET id = 0 WHERE Menu.id = $menuId';
+  const values = {$menuId: req.params.menuId};
+
+  db.run(sql, values, (error) => {
     if (error) {
       next(error);
-    } else if (issue) {
-      res.sendStatus(400);
     } else {
-      const deleteSql = 'DELETE FROM Menu WHERE Menu.id = $menuId';
-      const deleteValues = {$menuId: req.params.menuId};
-
-      db.run(deleteSql, deleteValues, (error) => {
-        if (error) {
-          next(error);
-        } else {
-          res.sendStatus(204);
-        }
-      });
+      db.get(`SELECT * FROM Menu WHERE Menu.id = ${req.params.menuId}`,
+        (error, menu) => {
+          res.status(200).json({menu: menu});
+        });
     }
   });
 });
 
-module.exports = menuRouter;
+module.exports = menusRouter;
